9495b93bc6ebe75eab587195e6245377c755a8f7
Stanislav Kucherenko <svkwork1@gmail.com>
2026-01-19 21:32:33 +0100

    set up main menu

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-def main(): ...
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static, Button
+from textual.containers import ScrollableContainer
+
+
+class MainMenu(Static):
+    """Main menu widget."""
+
+    @on(Button.Pressed, "#quit-btn")
+    def handle_exit_button(self) -> None:
+        self.app.exit()
+
+    def compose(self) -> ComposeResult:
+        yield Button("Start Game", id="start-game-btn", variant="primary")
+        yield Button("View Scores", id="view-scores-btn", variant="primary")
+        yield Button("Exit Game", id="quit-btn", variant="error")
+
+
+class DiceGame(App):
+    """A dice game application."""
+
+    CSS_PATH = "./styles/main.tcss"
+
+    BINDINGS = [("d", "toggle_dark", "Theme"), ("q", "quit", "Quit")]
+
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        yield ScrollableContainer(
+            Static("Welcome to the Dice Game!", id="welcome-message"),
+            MainMenu(id="main-menu"),
+        )
+        yield Footer()
 
 
 if __name__ == "__main__":
-    main()
+    app = DiceGame()
+    app.run()
