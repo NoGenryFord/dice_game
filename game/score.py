fa18d7cebb07857b5a238178e336a56d7eae1e57
Stanislav Kucherenko <svkwork1@gmail.com>
2026-01-21 22:44:11 +0100

    Implement save_result method to store game results in JSON format and add game_screen styles

--- game/score.py
+++ game/score.py
@@ -6,7 +6,24 @@ class FileController:
 
     __ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-    def save_result(self, name: str, round: int, score: int): ...
+    def save_result(self, date: str, player: str, rounds: int, score: int):
+        file_path = os.path.join(self.__ROOT_PATH, "..", "scores", "games_result.json")
+
+        if os.path.exists(file_path):
+            with open(file=file_path, mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+        else:
+            data = []
+
+        new_result = {
+            "Дата": date,
+            "Игрок": player,
+            "Количество раундов": rounds,
+            "Итоговый счет": score,
+        }
+        data.append(new_result)
+        with open(file=file_path, mode="w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
     def read_results(self):
         result = []
