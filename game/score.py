d881e2fcddce35de6427e29e8ec58d7200b015b8
Stanislav Kucherenko <svkwork1@gmail.com>
2026-01-21 22:16:25 +0100

    Refactor file path handling for score results and update score screen to display scores from JSON

--- game/score.py
+++ game/score.py
@@ -4,16 +4,15 @@ import os
 
 class FileController:
 
-    __BASE_PATH = os.path.dirname("/scores/game_results.json")
+    __ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
     def save_result(self, name: str, round: int, score: int): ...
 
     def read_results(self):
         result = []
+        file_path = os.path.join(self.__ROOT_PATH, "..", "scores", "games_result.json")
 
-        with open(
-            file="./scores/game_results.json", mode="r", encoding="utf-8"
-        ) as file:
+        with open(file=file_path, mode="r", encoding="utf-8") as file:
             data = json.load(file)
             for item in data:
                 result.append(
--- ui/score_screen.py
+++ ui/score_screen.py
@@ -1,14 +1,20 @@
 from textual import on
 from textual.app import ComposeResult
-from textual.widgets import Header, Footer, Static, Button
+from textual.widgets import Header, Footer, Static, Button, DataTable
 from textual.containers import ScrollableContainer, Container
 from textual.screen import Screen
+
+
+from game.score import FileController
 
 
 class ScoreScreen(Screen):
     """Screen to display scores."""
 
     CSS_PATH = "../styles/score_screen.tcss"
+
+    file_controller = FileController()
+    score_parser = file_controller.read_results()
 
     @on(Button.Pressed, "#back-btn")
     def handle_back_button(self) -> None:
@@ -18,8 +24,15 @@ class ScoreScreen(Screen):
         yield Header(show_clock=True)
         yield Container(
             Static("Scores Table", id="scores-header"),
-            ScrollableContainer(id="scores-table"),
+            ScrollableContainer(
+                DataTable(id="scores-table"), id="scores-table-container"
+            ),
             Button("Back to Main Menu", id="back-btn", variant="primary"),
             id="score-container",
         )
         yield Footer()
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.add_columns("Дата", "Игрок", "Количество раундов", "Итоговый счет")
+        table.add_rows(self.score_parser)
