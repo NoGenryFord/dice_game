d99cadeeb38fdf37b13d36826cbf7d090d5faa99
Stanislav Kucherenko <svkwork1@gmail.com>
2026-01-24 18:13:41 +0100

    Refactor Player class to encapsulate attributes and improve name handling

--- game/models.py
+++ game/models.py
@@ -1,6 +1,16 @@
 class Player:
+    __name: str
+    __score: int
+    __turn: int
+
     def __init__(self, name: str = "Default User") -> None:
-        self.name = name
+        self.__name = name
+
+    def __str__(self) -> str:
+        return self.__name
+
+    def set_name(self, name: str) -> None:
+        self.__name = name
 
 
 if __name__ == "__main__":
